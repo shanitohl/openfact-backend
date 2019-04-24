83e2869224960d77dfa7869c9ba21768fdfc4158
ChanoHL <shanohl.sist@gmail.com>
2019-04-24 15:57:26 -0500

    aaa

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+docker build -t shanohl/openfact-backend .
 docker tag shanohl/openfact-backend shanohl/openfact-backend:$(git rev-parse --short HEAD)
 docker push shanohl/openfact-backend:$(git rev-parse --short HEAD)
 # docker push shanohl/openfact-backend:lastest
