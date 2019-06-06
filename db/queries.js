a95b0e9dc892d31a7fa37449d8f9b9668bb8feac
ChanoHL <shanohl.sist@gmail.com>
2019-06-06 15:37:04 -0500

    change datos

--- db/queries.js
+++ db/queries.js
@@ -9,20 +9,31 @@ function getQuerySelectAllDocuments(organization_id, filter) {
 
     filter.filters.forEach(element => {
         if (element.name == "createdTimestamp")
-            queryWhere = queryWhere + " and createdTimestamp > '" + element.value + "'";
+            queryWhere = queryWhere + " and created_timestamp > '" + element.value + "'";
+        if (element.name == "documentId")
+            queryWhere = queryWhere + " and d.document_id like '" + element.value.replace("*", "") + "%'";
         if (element.name == "document_type")
             queryWhere = queryWhere + " and document_type='" + element.value + "'";
         if (element.name == "status")
             queryWhere = queryWhere + " and status='" + element.value + "'";
-        if (element.name == "required_action")
+        if (element.name == "requiredActions")
             queryWhere = queryWhere + " and required_action='" + element.value + "'";
     });
-    if (filter.filterText != "")
-        queryWhere = queryWhere + "and (a.value like '%" + filter.filterText + "%'  or d.customer_assigned_account_id like '%" + filter.filterText + "%' or d.customer_registration_name like '%" + filter.filterText + "%')"
-    if (filter.paging != null)
-        queryWhere = queryWhere + "LIMIT  " + filter.paging.pageSize + " OFFSET  " + filter.paging.page + " ;"
+    if (filter.filterText != null)
+        queryWhere = queryWhere + " and (a.value like '%" + filter.filterText + "%'  or d.document_id like '%" + filter.filterText + "%'  or d.customer_assigned_account_id like '%" + filter.filterText + "%' or d.customer_registration_name like '%" + filter.filterText + "%')"
 
-    //console.log(querySelect + " " + queryWhere);
+    filter.orders.forEach(element => {
+        if (element.name == "createdTimestamp") {           
+                if (element.ascending)
+                    queryWhere = queryWhere + " order by d.created_timestamp asc";
+                else
+                    queryWhere = queryWhere + " order by d.created_timestamp desc ";            
+        }
+    });
+    if (filter.paging != null)
+        queryWhere = queryWhere + " LIMIT  " + filter.paging.pageSize + " OFFSET  " + filter.paging.page + " ;"
+
+    console.log(querySelect + " " + queryWhere);
     return querySelect + " " + queryWhere;
 }
 
@@ -43,8 +54,8 @@ function getQueryReportVentas(organization_id, dateFrom, dateTo) {
                     doc.customer_registration_name,
                     ''  as exportacion,
                     a.value                             as gravada,                 
-                    0                                   as exonerada,
-                    0                                   as inafecta,
+                    opExonerada.value                   as exonerada,
+                    opInafecta.value                    as inafecta,
                     0                                   as ValorIsc,                    
                     b.value                             as valorIgv,
                     0                                   as otros_tributos,
@@ -67,6 +78,8 @@ function getQueryReportVentas(organization_id, dateFrom, dateTo) {
                 left join document_attribute c on doc.id = c.document_id and c.name = 'legalMonetaryTotalPayableAmount'
                 left join document_attribute tipo_doc_client on doc.id = tipo_doc_client.document_id and tipo_doc_client.name = 'customerPartyAdditionalAccountID'
                 left join document_attribute d on doc.id = d.document_id and d.name = 'invoiceTypeCode'
+                left join document_attribute opInafecta on doc.id = opInafecta.document_id and a.name = 'totalOperacionesInafectas'
+                left join document_attribute opExonerada on doc.id = opExonerada.document_id and a.name = 'totalOperacionesExoneradas'
               where doc.organization_id =  '` + organization_id + `' and  doc.document_type in ('INVOICE','CREDIT_NOTE','DEBIT_NOTE')
               and doc.issue_date BETWEEN to_date('` + dateFrom + `','YYYY-MM-DD') AND to_date('` + dateTo + `','YYYY-MM-DD') order by doc.issue_date desc;`;
     return query;
